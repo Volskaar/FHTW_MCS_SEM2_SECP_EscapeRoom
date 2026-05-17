da9e37b08edb22559e2bb28af208a833776ae993
volskaar <pusam.raphy@gmail.com>
2026-05-17 19:51:35 +0200

    added IMPORTANT folder with .emls from conversation and password file

--- init/1-copy-files.sh
+++ init/1-copy-files.sh
@@ -5,6 +5,9 @@ CONFIG_PATH="/config/.custom-config"
 mkdir $CONFIG_PATH
 cp -a /webtop_configurations/. $CONFIG_PATH/
 
+mkdir /config/Desktop/IMPORTANT
+cp -a /webtop_configurations/email /config/Desktop/IMPORTANT/email
+cp /webtop_configurations/passwords.txt /config/Desktop/IMPORTANT/passwords.txt
 
 chmod +x $CONFIG_PATH/set-wallpaper.sh
 chown abc:abc $CONFIG_PATH/set-wallpaper.sh
